8cbc760dce9f3b110b13027b3cb2cc0846d191a4
saberpl <planhbang@gmail.com>
2020-06-11 14:45:04 +0700

    Updates

---
+++ precache-manifest.05b561b19f0f95a1440e44093edd1702.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a1e5cd8aa2aca226b7c63dca8c16c4",
+    "url": "/livolin/index.html"
+  },
+  {
+    "revision": "12afa3c6972bcbd41731",
+    "url": "/livolin/static/css/2.885cdae4.chunk.css"
+  },
+  {
+    "revision": "d89238dbe58e3fac52bf",
+    "url": "/livolin/static/css/main.1350aa60.chunk.css"
+  },
+  {
+    "revision": "12afa3c6972bcbd41731",
+    "url": "/livolin/static/js/2.e173b39e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/livolin/static/js/2.e173b39e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d89238dbe58e3fac52bf",
+    "url": "/livolin/static/js/main.0772e6e2.chunk.js"
+  },
+  {
+    "revision": "6e5c7a38ae643a8e6628",
+    "url": "/livolin/static/js/runtime-main.28829fd8.js"
+  },
+  {
+    "revision": "e78626a1bb6f0065e209c4ee6abe9b78",
+    "url": "/livolin/static/media/ask.e78626a1.png"
+  },
+  {
+    "revision": "cee73e7b965d80807210b03da0eb5b8d",
+    "url": "/livolin/static/media/banner.cee73e7b.jpg"
+  },
+  {
+    "revision": "88242ef22e7762882b7e231d052aa89c",
+    "url": "/livolin/static/media/comment.88242ef2.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/livolin/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/livolin/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/livolin/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/livolin/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/livolin/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "951e94461b9382bf9b43d071ff303ff9",
+    "url": "/livolin/static/media/handbook1.951e9446.png"
+  },
+  {
+    "revision": "ebc601b2999ef285efc91937ebeb3f9c",
+    "url": "/livolin/static/media/handbook2.ebc601b2.png"
+  },
+  {
+    "revision": "9bb06913bd66b157a14db1fc0a867d90",
+    "url": "/livolin/static/media/handbook3.9bb06913.jpg"
+  },
+  {
+    "revision": "0b436349d2450284812ff6b86d4bb237",
+    "url": "/livolin/static/media/handbook4.0b436349.png"
+  },
+  {
+    "revision": "44cc6a4778a4c9751740f785da053c63",
+    "url": "/livolin/static/media/livolin.44cc6a47.png"
+  },
+  {
+    "revision": "a6b5be0027feec420a40855c46e36094",
+    "url": "/livolin/static/media/logo.a6b5be00.png"
+  },
+  {
+    "revision": "a7be73bf635f1c9d82cfc29bc12da9b9",
+    "url": "/livolin/static/media/news1.a7be73bf.png"
+  },
+  {
+    "revision": "3d6b2751d64a94cb6f13b4e78b7c7905",
+    "url": "/livolin/static/media/news2.3d6b2751.png"
+  },
+  {
+    "revision": "7dc03787dd810f23a8757c4d9913d4d9",
+    "url": "/livolin/static/media/news3.7dc03787.png"
+  },
+  {
+    "revision": "6c667ca243f444bf5711f2659a3c5cf2",
+    "url": "/livolin/static/media/share1.6c667ca2.png"
+  },
+  {
+    "revision": "0cd97a9a00b0f0f312dd07d8a49cdf76",
+    "url": "/livolin/static/media/share2.0cd97a9a.png"
+  },
+  {
+    "revision": "cc80d07aa483cb1ea9ec62843f85cdae",
+    "url": "/livolin/static/media/share3.cc80d07a.png"
+  },
+  {
+    "revision": "1af23438ab73fdefaec3c1eb724d8b33",
+    "url": "/livolin/static/media/share4.1af23438.png"
+  }
+]);
